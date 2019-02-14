6b4b4ea5bb40129c624c2c48b3df38f198ecfb9f
vidul <vidul.verma@outlook.com>
2019-02-14 11:22:24 -0800

    filecopy and generate artifactid

--- gitcommands.js
+++ gitcommands.js
@@ -1,13 +1,89 @@
 var ncp = require('ncp').ncp;
+let fs = require('fs');
+let path = require('path')
 
 const push = (sourcePath) => {
     let dest = __dirname + "/repos/";
-    ncp(sourcePath, dest, function (err) {
-        if (err) {
-            return console.error(err);
+    let sourceArray = sourcePath.split('\\');
+    let sourceFolderName = sourceArray[sourceArray.length - 1]
+    let destDir = dest + sourceFolderName;
+    if (!fs.existsSync(destDir))
+        fs.mkdir(destDir);
+
+    parseDirectory(sourcePath, destDir)
+
+
+    // ncp(sourcePath, dest, function (err) {
+    //     if (err) {
+    //         return console.error(err);
+    //     }
+    //     console.log('done!');
+    // });
+}
+
+function parseDirectory(sourceDir, destDir) {
+
+    let files = fs.readdirSync(sourceDir);
+    let tempDir = destDir;
+    for (file in files) {
+        let next = path.join(sourceDir, files[file]);
+        if (fs.lstatSync(next).isDirectory() == true) {
+            destDir = tempDir + "/" + files[file];
+            if (!fs.existsSync(destDir)) {
+                fs.mkdirSync(destDir);
+            }
+            parseDirectory(next, destDir);
+        } else {
+            //create folder with filename
+            destDir = tempDir + "/" + files[file];
+            if (!fs.existsSync(destDir)) {
+                fs.mkdirSync(destDir);
+            }
+            createFileWithArtifactId(next, files[file], destDir)
+
+            // let myVal = getArtifactId(next, files[x], mainDir)
+            // myVal.then((filename) => {
+            //     //console.log(filename);
+
+            // })
+
+
+
         }
-        console.log('done!');
+    }
+}
+
+function createFileWithArtifactId(sourcePath, fileName, destPath) {
+
+    fs.readFile(sourcePath, 'utf8', function (err, contents) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("--------------" + fileName + "----------" + destPath);
+            let artifactId = calculateArtifactId(sourcePath, contents)
+            fs.appendFile(destPath + "/" + artifactId, contents, (err) => {
+                if (err)
+                    console.log(err);
+            });
+        }
     });
+
+}
+
+function calculateArtifactId(sourcePath, contents) {
+
+    let extension = sourcePath.split(".");
+    let len = contents.length;
+    let weights = [1, 7, 3, 7, 11];
+    let checkSum = 0;
+    let j = 0;
+    for (let i = 0; i < len; i++) {
+        j = j % weights.length;
+        checkSum += weights[j++] * contents.charCodeAt(i);
+    }
+    let modulus = Math.pow(2, 31) - 1;
+    checkSum = checkSum % modulus;
+    return `${checkSum}-L${len}.${extension[1]}`;
 }
 
 module.exports = {
